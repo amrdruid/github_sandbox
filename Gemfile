381bd1d1e1e894981f74a91317ceb2a9bb7cc4f7
druid <amrdruid@gmail.com>
2016-09-30 21:11:25 +0200

    First commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+ruby '2.3.0'
+
+gem 'rails', '~> 5.0.0.1'
+gem 'sqlite3'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+
+# Ruby toolkit for the GitHub API
+gem "octokit", "~> 4.0"
+
+group :development, :test do
+  gem 'byebug'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+  gem 'rails_layout'
+end
---
+++ app/controllers/sandbox_controller.rb
@@ -0,0 +1,33 @@
+class SandboxController < ApplicationController
+  def login(username, password)
+    client = Octokit::Client.new(:login => username, :password => password)
+    begin
+      user = client.user
+			puts "Welcome, #{user.name}"
+			user
+		rescue => error
+      puts "Failed, either username or password are incorrect"
+      return
+    end
+  end
+
+	def repos_languages(username)
+		user = Octokit.user username
+		data = {}
+		begin
+			repos = user.rels[:repos].get.data
+			repos.each do |repo|
+				if data[repo.language].nil?
+					data[repo.language] = 1
+				else
+					data[repo.language] += 1
+				end
+			end
+			data.sort_by{|_key, value| value}
+			key, value = data.first
+			puts "#{user.name} can highly be #{key} as he used it in #{value} repos"
+		rescue => error
+			puts "Failed, username is incorrect"
+		end
+	end
+end
